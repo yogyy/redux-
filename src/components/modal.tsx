c9a27652c160de05074b9f48db31f43516f87464
yogyy <yogimr261@gmail.com>
2024-05-29 18:52:21 +0700

    feat: add modal

---
+++ src/components/modal.tsx
@@ -0,0 +1,72 @@
+import {
+  Button,
+  Dialog,
+  DialogPanel,
+  DialogTitle,
+  Transition,
+  TransitionChild,
+} from "@headlessui/react";
+import { useState } from "react";
+
+export function Modal({
+  children,
+  title,
+  content,
+}: {
+  children: React.ReactNode;
+  title: React.ReactNode;
+  content: React.ReactNode;
+}) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function open() {
+    setIsOpen(true);
+  }
+
+  function close() {
+    setIsOpen(false);
+  }
+
+  return (
+    <>
+      <Button
+        onClick={open}
+        className="rounded-md bg-black/20 px-4 py-2 text-sm font-medium text-white focus:outline-none data-[hover]:bg-black/30 data-[focus]:outline-1 data-[focus]:outline-white"
+      >
+        {children}
+      </Button>
+
+      <Transition appear show={isOpen}>
+        <Dialog
+          as="div"
+          className="relative z-10 focus:outline-none"
+          onClose={close}
+          __demoMode
+        >
+          <div className="fixed inset-0 z-10 w-screen overflow-y-auto backdrop-blur-sm transition-all duration-300">
+            <div className="flex min-h-full items-center justify-center p-4">
+              <TransitionChild
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 transform-[scale(95%)]"
+                enterTo="opacity-100 transform-[scale(100%)]"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 transform-[scale(100%)]"
+                leaveTo="opacity-0 transform-[scale(95%)]"
+              >
+                <DialogPanel className="w-full max-w-md rounded-xl bg-white/5 p-6 backdrop-blur-2xl">
+                  <DialogTitle
+                    as="h3"
+                    className="text-base/7 font-medium text-white"
+                  >
+                    {title}
+                  </DialogTitle>
+                  {content}
+                </DialogPanel>
+              </TransitionChild>
+            </div>
+          </div>
+        </Dialog>
+      </Transition>
+    </>
+  );
+}
