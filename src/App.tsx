b8103c1d1eac334a9c5fcb031ee38f446ba41f11
yogyy <yogimr261@gmail.com>
2024-06-01 20:09:29 +0700

    chore: remove error

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import { TodoCollection } from "./components/todo";
-import { Speech } from "./components/web-speech";
 
 function App() {
   return (
     <>
       <TodoCollection />
-      <Speech />
     </>
   );
 }
